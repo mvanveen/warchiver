8e261b7edaedc31b53d652bd49f6827b41b2b73a
Michael Van Veen <michael@mvanveen.net>
2014-04-15 21:18:09 -0700

    add option parsing

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+from optparse import OptionParser
 import os
 
 import bottle
@@ -8,6 +9,19 @@ import requests
 STATIC_PATH = os.path.abspath(
   os.path.join(os.path.abspath(__file__), '../')
 )
+
+def get_options():
+  parser = OptionParser()
+  parser.add_option(
+    '--host', dest='hostname', help='source url hostname'
+  )
+  parser.add_option(
+    '-f', '--filename', dest='filename', help='source url hostname',
+    default='index.html'
+  )
+  (options, args) = parser.parse_args()
+  return options, args
+
 
 @app.route('/')
 def serve_static():
@@ -30,10 +44,16 @@ def serve_static(path):
     os.makedirs(folder)
 
   with open(os.path.abspath(STATIC_PATH + path), 'w') as file_obj:
-    response = requests.get('%s%s' % (host, path))
+    response = requests.get('%s%s' % (options.hostname, path))
     print 'writing %s...' % (path,)
     file_obj.write(response.content)
-  #import pdb; pdb.set_trace()
 
-webapp = bottle.default_app()
-webapp.run(host='0.0.0.0', port=9999)
+
+options, args = get_options()
+if __name__ == '__main__':
+
+  if not options.hostname:
+    raise ValueError('hostname is required')
+
+  webapp = bottle.default_app()
+  webapp.run(host='0.0.0.0', port=9999)
